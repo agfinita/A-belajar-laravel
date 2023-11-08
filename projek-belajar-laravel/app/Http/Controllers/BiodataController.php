c585bdafa3a3d2ec5d28a648e874dd85b3effdd8
agfinita <agfinitaagh@gmail.com>
2023-11-08 23:15:02 +0700

    [DOCS] add controller for views biodata&hello

---
+++ projek-belajar-laravel/app/Http/Controllers/BiodataController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BiodataController extends Controller
+{
+    //
+    public function bio() {
+        return view('biodata');
+    }
+}
