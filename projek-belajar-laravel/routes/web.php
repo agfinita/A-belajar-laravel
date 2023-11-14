23d387c7335246e81508d31c4a0ce05674afaf1a
agfinita <agfinitaagh@gmail.com>
2023-11-14 12:24:07 +0700

    [DOCS] edit file routes

--- projek-belajar-laravel/routes/web.php
+++ projek-belajar-laravel/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\ShowProductController;
+use App\Http\Controllers\AddProductController;
 use App\Http\Controllers\AdminLTEController;
 use App\Http\Controllers\BiodataController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SearchProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,4 +35,21 @@ Route::get('/hello', [HelloController::class, 'helo']);
 
 Route::get('/biodata', [BiodataController::class, 'bio']);
 
-Route::get('/adminLTE/master', [AdminLTEController::class, 'tampil']);
+Route::get('/master', [AdminLTEController::class, 'tampil']);
+
+// Create product
+Route::get('/master/create', [AddProductController::class, 'addShow']);
+// Display product
+Route::get('/master/table', [ShowProductController::class, 'readShow']);
+// Post form create product
+Route::post('/master/table', [ShowProductController::class, 'addProcess']);
+// Update product
+Route::get('/master/edit/{id}', [ShowProductController::class, 'update']);
+Route::patch('/master/edit/{id}', [ShowProductController::class, 'updateProcess']);
+// Delete product
+Route::delete('/master/table/{id}', [ShowProductController::class, 'delete']);
+// Fitur search
+Route::get('/master/search', [SearchProductController::class, 'search']);
+
+
+Route::get('/master/table/category', [CategoryController::class, 'index']);
